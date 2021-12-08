5be768a476dac7e49a7e729f20f44f0f3ccdfd55
Jessica Stern <jlstern3@gmail.com>
2021-12-07 21:12:45 -0500

    Create a new blog post pg properly showing

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path('', views.index),
+    path('blog/new', views.new_blog_post),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'index.html')
+
+def new_blog_post(request):
+    # if 'user_id' not in request.session: 
+    #     return redirect('/')
+    # else: 
+    return render(request, 'new_blog_post.html')
